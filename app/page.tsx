d151a319e2e4ee005089c81e3657006eab93650f
Thomas FitzGerald <tomrfitz@gmail.com>
2023-04-24 21:44:28 -0400

    add testimonials to home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 "use client";
 import Features from "@/components/Features";
 import HeroSection from "@/components/HeroSection";
+import Testimonials from "@/components/Testimonials";
 
 export default function Home() {
   return (
     <>
       <HeroSection />
       <Features />
+      <Testimonials />
     </>
   );
 }
